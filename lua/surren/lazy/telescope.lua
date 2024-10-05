dcc01c45e809aa4682c27afdd7f131764114bdc0
Surren Deorajsingh <surren@surrenity.com>
2024-10-05 15:36:16 -0500

    Update telescope.lua

--- lua/surren/lazy/telescope.lua
+++ lua/surren/lazy/telescope.lua
@@ -17,6 +17,9 @@ return {
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>ps', function ()
+        buildin.grep_string({ search = vim.fn.input("rg > ") });
+      end)
 
     end 
 }
