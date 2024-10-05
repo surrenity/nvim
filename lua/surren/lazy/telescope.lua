692c0070c399cedc37ae0552da06a9f7b4bb630f
Surren Deorajsingh <surren@surrenity.com>
2024-10-05 15:39:47 -0500

    Update telescope.lua

--- lua/surren/lazy/telescope.lua
+++ lua/surren/lazy/telescope.lua
@@ -14,7 +14,7 @@ return {
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<C-f>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       vim.keymap.set('n', '<leader>ps', function ()
