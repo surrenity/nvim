9aac0c583fdc6428f2c508d7554109874ce41d41
Surren Deorajsingh <surren@surrenity.com>
2024-10-21 09:55:51 -0500

    Add buffer next Buffer previous
    Mapping 
    Ctrl-n to next buffer
    Ctrl-p to prev buffer

--- lua/surren/remap.lua
+++ lua/surren/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("x", "<leader>p", [["_dp]])
+vim.keymap.set("n", "<C-n>", ":bnext<CR>")
+vim.keymap.set("n", "<C-p>", ":bprev<CR>")
